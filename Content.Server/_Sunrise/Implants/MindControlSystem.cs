904f09e7ba4e2812bed167ccbbd7c7182ffdcd2c
KaiserMaus <kaiser.ratte@gmail.com>
2026-06-09 19:06:23 +0100

    review

--- Content.Server/_Sunrise/Implants/MindControlSystem.cs
+++ Content.Server/_Sunrise/Implants/MindControlSystem.cs
@@ -1,107 +1,46 @@
 using Content.Server.Antag;
 using Content.Server.Objectives;
 using Content.Server.Popups;
+using Content.Shared._Sunrise.Implants;
 using Content.Shared._Sunrise.Implants.Components;
-using Content.Shared.Bed.Sleep;
-using Content.Shared.Damage.Components;
-using Content.Shared.Damage.Systems;
-using Content.Shared.Emp;
-using Content.Shared.Implants;
 using Content.Shared.Mind;
-using Content.Shared.Mindshield.Components;
 using Content.Shared.Popups;
-using Content.Shared.StatusEffectNew;
 using Robust.Shared.Player;
 
 namespace Content.Server._Sunrise.Implants;
 
-public sealed class MindControlSystem : EntitySystem
+public sealed class MindControlSystem : SharedMindControlSystem
 {
     private const string FollowOrdersObjectiveId = "MindControlledFollowOrders";
 
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly ObjectivesSystem _objectives = default!;
     [Dependency] private readonly SharedMindSystem _mind = default!;
-    [Dependency] private readonly StatusEffectsSystem _status = default!;
     [Dependency] private readonly AntagSelectionSystem _antag = default!;
-    [Dependency] private readonly SharedStaminaSystem _stamina = default!;
 
-    public override void Initialize()
+    protected override bool CanApplyMindControl(Entity<MindControlImplantComponent> ent, EntityUid implanted, string masterName)
     {
-        base.Initialize();
-        SubscribeLocalEvent<MindControlImplantComponent, AddImplantAttemptEvent>(OnAttemptImplant);
-        SubscribeLocalEvent<MindControlImplantComponent, ImplantImplantedEvent>(OnImplanted);
-        SubscribeLocalEvent<MindControlImplantComponent, ImplantRemovedEvent>(OnRemoved);
-        SubscribeLocalEvent<MindControlComponent, EmpPulseEvent>(OnEmpPulse);
+        return HasComp<ActorComponent>(implanted);
     }
 
-    private void OnAttemptImplant(Entity<MindControlImplantComponent> ent, ref AddImplantAttemptEvent args)
+    protected override void SendImplantedBriefing(Entity<MindControlImplantComponent> ent, EntityUid implanted, string masterName)
     {
-        if (args.User == args.Target)
-        {
-            args.Cancel();
-            return;
-        }
-
-        if (!_mind.TryGetMind(args.Target, out _, out var targetMind) || _mind.IsCharacterDeadIc(targetMind))
-        {
-            _popup.PopupEntity(Loc.GetString("mind-control-invalid"), args.User, args.User, PopupType.Small);
-            args.Cancel();
-            return;
-        }
-
-        if (HasComp<MindShieldComponent>(args.Target))
-        {
-            _popup.PopupEntity(Loc.GetString("mind-control-prevented"), args.User, args.User, PopupType.Small);
-            args.Cancel();
-            return;
-        }
-
-        ent.Comp.Master = args.User;
-    }
-
-    private void OnImplanted(Entity<MindControlImplantComponent> ent, ref ImplantImplantedEvent args)
-    {
-        if (!TryComp<ActorComponent>(args.Implanted, out var actor))
+        if (!TryComp<ActorComponent>(implanted, out var actor))
             return;
 
-        if (!_mind.TryGetMind(ent.Comp.Master, out _, out var masterMind) || masterMind.CharacterName == null)
-            return;
-
-        EnsureComp<MindControlComponent>(args.Implanted);
         _antag.SendBriefing(actor.PlayerSession,
-            Loc.GetString(ent.Comp.BriefingText, ("master-name", masterMind.CharacterName)),
+            Loc.GetString(ent.Comp.BriefingText, ("master-name", masterName)),
             null,
-            ent.Comp.BriefingSound);
-
-        _status.TryAddStatusEffectDuration(args.Implanted, SleepingSystem.StatusEffectForcedSleeping, TimeSpan.FromSeconds(2));
-        AssignObjective(args.Implanted);
+            ent.Comp.GreetSoundNotification);
     }
 
-    private void OnRemoved(Entity<MindControlImplantComponent> ent, ref ImplantRemovedEvent args)
+    protected override void SendRemovedBriefing(Entity<MindControlImplantComponent> ent, EntityUid implanted)
     {
-        if (TerminatingOrDeleted(args.Implanted))
-            return;
-
-        if (TryComp<ActorComponent>(args.Implanted, out var actor))
+        if (TryComp<ActorComponent>(implanted, out var actor))
             _antag.SendBriefing(actor.PlayerSession, Loc.GetString(ent.Comp.DebriefingText), null, null);
-
-        RemoveObjective(args.Implanted);
-        RemCompDeferred<MindControlComponent>(args.Implanted);
-        _status.TryAddStatusEffectDuration(args.Implanted, SleepingSystem.StatusEffectForcedSleeping, TimeSpan.FromSeconds(2));
     }
 
-    private void OnEmpPulse(Entity<MindControlComponent> ent, ref EmpPulseEvent args)
-    {
-        if (!TryComp<StaminaComponent>(ent.Owner, out var stamina))
-            return;
-
-        _stamina.TakeStaminaDamage(ent.Owner, stamina.CritThreshold, stamina);
-        args.Affected = true;
-        args.Disabled = true;
-    }
-
-    private void RemoveObjective(EntityUid uid)
+    protected override void RemoveObjective(EntityUid uid)
     {
         if (!_mind.TryGetMind(uid, out var mindId, out var mind))
             return;
@@ -117,7 +56,7 @@ public sealed class MindControlSystem : EntitySystem
         _popup.PopupEntity(Loc.GetString("mind-control-user-freed"), uid, uid, PopupType.Medium);
     }
 
-    private void AssignObjective(EntityUid uid)
+    protected override void AssignObjective(EntityUid uid)
     {
         if (!_mind.TryGetMind(uid, out var mindId, out var mind))
             return;
--- Content.Shared/_Sunrise/Implants/Components/MindControlImplantComponent.cs
+++ Content.Shared/_Sunrise/Implants/Components/MindControlImplantComponent.cs
@@ -8,7 +8,8 @@ namespace Content.Shared._Sunrise.Implants.Components;
 [RegisterComponent]
 public sealed partial class MindControlImplantComponent : Component
 {
-    public EntityUid Master = EntityUid.Invalid;
+    [ViewVariables]
+    public EntityUid? Master;
 
     [DataField]
     public LocId BriefingText = "mind-control-user-briefing";
@@ -16,7 +17,15 @@ public sealed partial class MindControlImplantComponent : Component
     [DataField]
     public LocId DebriefingText = "mind-control-user-freed";
 
+    /// <summary>
+    /// Music played to the implanted player with the mind control briefing.
+    /// </summary>
     [DataField]
-    public SoundSpecifier BriefingSound = new SoundPathSpecifier("/Audio/Ambience/Antag/traitor_start.ogg");
-}
+    public SoundSpecifier GreetSoundNotification = new SoundPathSpecifier("/Audio/_Sunrise/Ambience/Antag/brainwashed.ogg");
 
+    /// <summary>
+    /// Forced sleep duration after implanting or removing the mind control implant.
+    /// </summary>
+    [DataField]
+    public TimeSpan ForcedSleepDuration = TimeSpan.FromSeconds(2);
+}
