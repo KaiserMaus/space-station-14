956b8728d21ffc768ab47678b73dd43d8b69f66c
KaiserMaus <kaiser.ratte@gmail.com>
2026-06-13 19:19:23 +0100

    Update SoilSystem.cs

--- Content.Server/_Sunrise/Soil/SoilSystem.cs
+++ Content.Server/_Sunrise/Soil/SoilSystem.cs
@@ -1,41 +1,54 @@
-﻿using Content.Server.Damage.Systems;
-using Content.Server.Movement.Systems;
+using Content.Server.Botany.Components;
+using Content.Server.Damage.Systems;
 using Content.Server.Popups;
-using Content.Shared.Damage.Systems;
 using Content.Shared.Interaction.Events;
-using Content.Shared.Maps;
 using Content.Shared.Popups;
-using Content.Shared.Tiles;
 using Robust.Server.GameObjects;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Physics;
 
 namespace Content.Server._Sunrise.Soil;
 
-/// <summary>
-/// Система для мешка с землей
-/// </summary>
 public sealed class SoilSystem : EntitySystem
 {
-    [Dependency] private readonly TransformSystem _transform = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly PopupSystem _popup = default!;
-    [Dependency] private readonly IEntityManager _entity = default!;
     [Dependency] private readonly StaminaSystem _stamina = default!;
+
+    private EntityQuery<PlantHolderComponent> _plantHolderQuery;
 
     public override void Initialize()
     {
+        base.Initialize();
+
+        _plantHolderQuery = GetEntityQuery<PlantHolderComponent>();
+
         SubscribeLocalEvent<SoilComponent, UseInHandEvent>(OnUseInHand);
     }
 
     private void OnUseInHand(Entity<SoilComponent> ent, ref UseInHandEvent args)
     {
         var xform = Transform(ent);
-        var grid = _transform.GetGrid((ent.Owner, xform));
-        if (grid == null)
+        if (xform.GridUid is not { } gridUid || !TryComp<MapGridComponent>(gridUid, out var grid))
         {
             _popup.PopupEntity(Loc.GetString(ent.Comp.PopupStringFailed), ent, args.User, PopupType.SmallCaution);
             return;
         }
 
-        var spawned = _entity.SpawnEntity(ent.Comp.SpawnPrototype, xform.Coordinates);
+        var tile = _map.TileIndicesFor(gridUid, grid, xform.Coordinates);
+        var coords = _map.ToCenterCoordinates(gridUid, tile, grid);
+
+        foreach (var entity in _lookup.GetEntitiesIntersecting(coords, LookupFlags.Dynamic | LookupFlags.Static))
+        {
+            if (!_plantHolderQuery.HasComp(entity))
+                continue;
+
+            _popup.PopupEntity(Loc.GetString(ent.Comp.PopupStringFailed), ent, args.User, PopupType.SmallCaution);
+            return;
+        }
+
+        Spawn(ent.Comp.SpawnPrototype, coords);
 
         var targetMeta = MetaData(ent);
         var userMeta = MetaData(args.User);
@@ -43,6 +56,6 @@ public sealed class SoilSystem : EntitySystem
         _popup.PopupEntity(Loc.GetString(ent.Comp.PopupStringSuccess, ("user", userMeta.EntityName), ("name", targetMeta.EntityName)), args.User, PopupType.LargeGreen);
         _stamina.TryTakeStamina(args.User, ent.Comp.StaminaDamage);
 
-        _entity.QueueDeleteEntity(ent.Owner);
+        QueueDel(ent);
     }
 }
